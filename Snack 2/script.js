04307f22e7cab659acfda537cbdc84f268c120e6
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-12 19:42:41 +0100

    creo i primi due array

---
+++ Snack 2/script.js
@@ -0,0 +1,11 @@
+// Scrivi una funzione che fonda due array presi come parametri
+// (date per scontato che abbiano lo stesso numero di elementi)
+// prendendo alternativamente gli elementi da uno e dall’altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+// Dichiaro i due array
+const arrayAcciaio1 = ["primo acciaio 1", "secondo acciaio 1", "terzo acciaio 1"];
+
+const arrayAcciaio2 = ["primo acciaio 2", "secondo acciaio 2", "terzo acciaio 2"];
+
+// 
