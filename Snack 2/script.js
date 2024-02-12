d5de7b3260f023bbce6254b5e0a04e3a838fac68
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-12 20:14:55 +0100

    creo un ciclo for pee unirli in un nuovo array

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -8,4 +8,17 @@ const arrayAcciaio1 = ["primo acciaio 1", "secondo acciaio 1", "terzo acciaio 1"
 
 const arrayAcciaio2 = ["primo acciaio 2", "secondo acciaio 2", "terzo acciaio 2"];
 
-// 
+// Creo la funzione che "fonderà" i due array
+
+function forgia() {
+
+    // Dichiaro l'array risultante
+    let acciaioDamasco = [];
+    
+    // Creo un ciclo for per unire i due array
+    for( let i = 0; i < arrayAcciaio1.length; i++){
+        acciaioDamasco.push(arrayAcciaio1[i], arrayAcciaio2[i]);
+    }
+   
+    console.log(acciaioDamasco)
+}
